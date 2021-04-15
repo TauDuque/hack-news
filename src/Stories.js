a4f4ff965a356db6d71a5001fc2bec4feaae59e6
TauDuque <hectauduque@gmail.com>
2021-04-15 12:48:51 -0300

    mais funcs

--- src/Stories.js
+++ src/Stories.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { isLoading, hits } = useGlobalContext();
+  const { isLoading, hits, removeStory } = useGlobalContext();
 
   if (isLoading) {
     return <div className="loading"></div>;
@@ -11,8 +11,32 @@ const Stories = () => {
   return (
     <section className="stories">
       {hits.map((story) => {
-        console.log(story);
-        return <article className="story">single story</article>;
+        const { objectID, title, num_comments, url, points, author } = story;
+        return (
+          <article key={objectID} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span>
+              {num_comments} comments
+            </p>{" "}
+            <div>
+              <a
+                href={url}
+                className="read-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                read more
+              </a>
+              <button
+                className="remove-btn"
+                onClick={() => removeStory(objectID)}
+              >
+                remove
+              </button>
+            </div>
+          </article>
+        );
       })}
     </section>
   );
